4de753f959771ba43e288c3d24bac1206403145b
Chugg05 <chuggnv05@gmail.com>
2026-05-29 16:36:01 +0700

    fix: format date time

--- src/shared/lib/format.ts
+++ src/shared/lib/format.ts
@@ -11,12 +11,17 @@ export const formatDate = (val?: string) => {
 export const formatDateTime = (val?: string) => {
   if (!val) return "_";
 
-  return new Date(val).toLocaleString("vi-VN", {
+  const resDate = new Date(val);
+  const date = resDate.toLocaleDateString("vi-VN", {
     day: "2-digit",
     month: "2-digit",
     year: "numeric",
+  });
+  const time = resDate.toLocaleTimeString("vi-VN", {
     hour: "2-digit",
     minute: "2-digit",
     hour12: false,
   });
+
+  return `${date} | ${time} `;
 };
