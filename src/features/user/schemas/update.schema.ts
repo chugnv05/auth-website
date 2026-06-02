6a9106f303dede8a97bad479cb1f5f2c4bcbf850
Chugg05 <chuggnv05@gmail.com>
2026-06-02 14:50:31 +0700

    fix: add trim()

--- src/features/user/schemas/update.schema.ts
+++ src/features/user/schemas/update.schema.ts
@@ -2,12 +2,15 @@ import { MESSAGES } from "@/shared/constants/messages";
 import z from "zod";
 
 export const updateMeSchema = z.object({
-  firstName: z.string().min(2, MESSAGES.user.firstName.invalid),
-  lastName: z.string().min(2, MESSAGES.user.lastName.invalid),
+  firstName: z.string().trim().min(2, MESSAGES.user.firstName.invalid),
+  lastName: z.string().trim().min(2, MESSAGES.user.lastName.invalid),
   gender: z.enum(["MALE", "FEMALE", "UNKNOWN", "OTHER"], { message: MESSAGES.user.dob.required }),
   dob: z.string().min(1, MESSAGES.user.dob.required),
-  phoneNumber: z.string().regex(/^[0-9]{10,11}$/, MESSAGES.user.phoneNumber.format),
-  email: z.string().email(MESSAGES.user.email.format),
+  phoneNumber: z
+    .string()
+    .trim()
+    .regex(/^[0-9]{10,11}$/, MESSAGES.user.phoneNumber.format),
+  email: z.string().trim().email(MESSAGES.user.email.format),
 });
 
 export type UpdateMeSchemaType = z.infer<typeof updateMeSchema>;
--- src/shared/config/sidebar.data.ts
+++ src/shared/config/sidebar.data.ts
@@ -1,5 +1,5 @@
 import { PATHS } from "@/app/router/paths";
-import { Key, User, UserKey } from "lucide-react";
+import { Key, OctagonMinus, User, UserKey } from "lucide-react";
 import type { IconType } from "react-icons/lib";
 
 export type SidebarItem = {
@@ -19,27 +19,20 @@ export const sidebarData: SidebarColumn[] = [
     icon: User,
     title: "User",
     to: PATHS.USER,
-    items: [
-      { label: "Temp 1", to: "" },
-      { label: "Temp 2", to: "" },
-      { label: "Temp 3", to: "" },
-      { label: "Temp 4", to: "" },
-    ],
   },
   {
     icon: UserKey,
     title: "Role",
     to: PATHS.ROLE,
-    items: [
-      { label: "Temp 1", to: "" },
-      { label: "Temp 2", to: "" },
-      { label: "Temp 3", to: "" },
-      { label: "Temp 4", to: "" },
-    ],
   },
   {
     icon: Key,
     title: "Permisison",
+    to: PATHS.PERMISSION,
+  },
+  {
+    icon: OctagonMinus,
+    title: "Temp",
     to: PATHS.PERMISSION,
     items: [
       { label: "Temp 1", to: "" },
